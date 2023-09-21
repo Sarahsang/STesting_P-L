0ce27aa502c63519d5bcad11e64140eb6fc5dbc8
Miao Sang <118147601+Sarahsang@users.noreply.github.com>
2023-09-21 23:22:23 +1200

    element

--- SeleniumLearning/01.py
+++ SeleniumLearning/01.py
@@ -1,14 +1,23 @@
 from selelnium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from time import time
 
 driver = webdriver.chrome()
 driver.get("https://www.youtube.com/")
 
+# 放大窗口
+driver.maximize_window()
+    
 #找输入框
 driver.find_element(By.CLASS_NAME, 'classname').send_keys("输入字符")
 
 #点击搜索
 driver.find_element(By.CLASS_NAME, 'classname').click()
 
+    
 #间隔时间
 time.sleep(3)
 
