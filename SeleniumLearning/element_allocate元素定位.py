ba151560d3de130f94ecc1ebe5bf3a220065175d
Miao Sang <118147601+Sarahsang@users.noreply.github.com>
2023-09-22 01:36:31 +1200

    update

--- SeleniumLearning/element_allocate元素定位.py
+++ SeleniumLearning/element_allocate元素定位.py
@@ -1,15 +1,16 @@
-from selelnium import webdriver
+from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 # 元素定位
 
 ''' id 定位
+因为唯一性, id是很直接快速的, 但是很多情况下没办法用id定位
 善用inspect
 
 '''
 
-driver = webdriver.chrome()
-driver.get("https://www.youtube.com")
-element = driver.find_element(By.ID, "search")
+driver = webdriver.Chrome()
+driver.get("https://www.baidu.com/")
+element = driver.find_element(By.ID, "kw")
 element.send_keys("selenium")
 driver.quit()
