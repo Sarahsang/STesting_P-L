b45e30849ef574537fde0f2cf7c90049bd10e56d
Miao Sang <118147601+Sarahsang@users.noreply.github.com>
2023-09-21 16:17:58 +1200

    Update 01.py

--- SeleniumLearning/01.py
+++ SeleniumLearning/01.py
@@ -5,3 +5,12 @@ driver.get("https://www.youtube.com/")
 
 #找输入框
 driver.find_element(By.CLASS_NAME, 'classname').send_keys("输入字符")
+
+#点击搜索
+driver.find_element(By.CLASS_NAME, 'classname').click()
+
+#间隔时间
+time.sleep(3)
+
+#关闭页面
+driver.close()
