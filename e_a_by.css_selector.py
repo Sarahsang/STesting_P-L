51208c764713a6e468e9fd74c31fb05686fa86dc
Miao Sang <118147601+Sarahsang@users.noreply.github.com>
2023-09-22 15:08:31 +1200

    Update e_a_by.css_selector.py

--- e_a_by.css_selector.py
+++ e_a_by.css_selector.py
@@ -4,25 +4,42 @@ from time import sleep
 # 元素定位
 
 ''' css selector 定位
+通过css样式选择器去定位
 用的比较多 因为id或者name等不一定有
 善用inspect
 
 '''
 
 driver = webdriver.Chrome()
-driver.get("https://www.bilibili.com")
+driver.get("https://www.baidu.com")
+# driver.get("https://www.bilibili.com")
 
 #窗口最大化
 driver.maximize_window()
 
-#唯一的input tag name
-driver.find_element(By.TAG_NAME, "input").send_keys("selenium")
+#css selector
+# 根据id属性定位 加#
+# driver.find_element(By.CSS_SELECTOR, "#kw").send_keys("selenium")
+# driver.find_element(By.CSS_SELECTOR, "#su").click()
 
-#多个 tag name 不建议，太多了
-driver.find_elements(By.CLASS_NAME, "input")[50].click()
+# Class 属性值进行定位
+# 加.
+# driver.find_element(By.CSS_SELECTOR, '.nav-search-input').send_keys('selenium')
+
+# name 属性值进行定位 其他属性值也是可以这样定位的
+# driver.find_element(By.CSS_SELECTOR, '[name="wd"]').send_keys("selenium")
+
+# 标签（href） 属性值进行定位
+driver.find_element(By.CSS_SELECTOR, 'a[href="http://image.baidu.com/"]').click()
+# 模糊匹配-包含
+driver.find_element(By.CSS_SELECTOR, 'a[href*="image.baidu.com"]').click()
+# 模糊匹配-匹配开头
+driver.find_element(By.CSS_SELECTOR, 'a[href^="http://image.baidu"]').click()
+# 模糊匹配-匹配结尾
+driver.find_element(By.CSS_SELECTOR, 'a[href$="image.baidu.com/"]').click()
 
 #sleep在做调试时特别有用
-sleep(3)
+sleep(8)
 
 #加不加都会关闭 但是代码完整性
 driver.quit()
