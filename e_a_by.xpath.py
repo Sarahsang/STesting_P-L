37552750625f3cb0f4c1ce34e973c329eee593c8
Miao Sang <118147601+Sarahsang@users.noreply.github.com>
2023-09-23 22:54:11 +1200

    Update e_a_by.xpath.py

--- e_a_by.xpath.py
+++ e_a_by.xpath.py
@@ -21,7 +21,32 @@ driver.find_element(By.XPATH, '/html/body/div/div/div[3]/a')
 driver.find_element(By.XPATH, '//input[@id="kw"]').send_keys("软件测试老白")
 
 # class中间有空格，需要全部写上
-driver.find_element(By.XPATH, '//a[@class="mnav c-font-normal c-color-t"]').click()
+driver.find_element(By.XPATH, '//a[@class="mnav c-font-normal c-color-t"]').send_keys("软件测试老白")
+
+#根据name属性定位
+driver.find_element(By.XPATH, '//input[@name="wd"]').send_keys("软件测试老白")
+
+#多个属性组合定位 用and 或 or
+driver.find_element(By.XPATH, '//input[@name="wd" and @class="s_ipt"]').send_keys("软件测试老白")
+
+#多组数据使用下标定位
+driver.find_element(By.XPATH, '//div[@id="s-top-left"]/a[3]').click()
+
+#由子元素定位父元素 ..代表上一层
+driver.find_element(By.XPATH, '//input[@id="kw"]/..')
+
+#根据文本内容定位 试一下两种 不确定哪个对
+driver.find_element(By.XPATH, '//a[contains(text(),"网盘")]') 
+driver.find_element(By.XPATH, '//a[contains(text()="网盘"]') # 可能是这个对
+
+
+# 最后一个子元素,last()
+driver.find_element(By.XPATH, '//a[@class="mnav c-font-normal c-color-t"][last()]') 
+
+#动态ID
+https://element.eleme.cn/#/zh-CN/component/cascader
+driver.find_element(By.XPATH, '//span[text() = "默认 click 触发子菜单"]/following-sibling::div/div/input') 
+
 
 #窗口最大化
 driver.maximize_window()
