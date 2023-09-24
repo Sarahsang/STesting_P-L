f2e2b3868652230186df92c2634c2c91f810716e
Miao Sang <118147601+Sarahsang@users.noreply.github.com>
2023-09-24 18:05:56 +1300

    Update e_a_by.xpath.py

--- e_a_by.xpath.py
+++ e_a_by.xpath.py
@@ -6,7 +6,23 @@ from time import sleep
 ''' XPATH 定位 
 XML
 善用inspect
-
+从根节点开始选取(绝对路径)	/html/div/
+从任意节点开始选取(相对路径)	//div，列举出所有div标签
+选取当前节点的父节点	//input/.. 会选取 input 的父节点
+选取属性，或者根据属性选取	
+使用id属性定位	//div[@id='id_value']
+使用class属性定位	//a[@class="mnav"]
+使用name属性定位	//div[@name='wd']
+多个属性定位	//input[@name="wd" and @class="s_ipt"]
+第n个元素，使用index定位	//div[@id="s-top-left"]/a[3]
+最后一个元素	//a[@class="mnav"][last()]
+属性包含某字段	//div[contains(@title,'text')]
+属性以某字段开头	//div[starts-with(@title,'text')]
+属性以某字段结尾	//div[ends-with(@title,'text')]
+文本包含	//a[contains(text(),"网盘")]
+文本等于	//span[text() = "菜单"]
+同级弟弟元素	//div[@id=='id']/following-sibling::div
+同级哥哥元素	//div[@id=='id']/preceding-sibling::div
 '''
 
 driver = webdriver.Chrome()
@@ -35,10 +51,8 @@ driver.find_element(By.XPATH, '//div[@id="s-top-left"]/a[3]').click()
 #由子元素定位父元素 ..代表上一层
 driver.find_element(By.XPATH, '//input[@id="kw"]/..')
 
-#根据文本内容定位 试一下两种 不确定哪个对
+#根据文本内容定位 
 driver.find_element(By.XPATH, '//a[contains(text(),"网盘")]') 
-driver.find_element(By.XPATH, '//a[contains(text()="网盘"]') # 可能是这个对
-
 
 # 最后一个子元素,last()
 driver.find_element(By.XPATH, '//a[@class="mnav c-font-normal c-color-t"][last()]') 
