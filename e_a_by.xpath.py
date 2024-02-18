3be0f32fde2d7ea7e81a483664d16b58e8d1e521
Miao Sang <118147601+Sarahsang@users.noreply.github.com>
2024-02-18 18:25:08 +1300

    Update e_a_by.xpath.py

--- e_a_by.xpath.py
+++ e_a_by.xpath.py
@@ -62,6 +62,7 @@ https://element.eleme.cn/#/zh-CN/component/cascader
 driver.find_element(By.XPATH, '//span[text() = "默认 click 触发子菜单"]/following-sibling::div/div/input') 
 
 
+
 #窗口最大化
 driver.maximize_window()
 
