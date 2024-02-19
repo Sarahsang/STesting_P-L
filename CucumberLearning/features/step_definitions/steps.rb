65ac6e3fbbb6ed08f501d8c48653f91fdec51c64
Miao Sang <118147601+Sarahsang@users.noreply.github.com>
2024-02-20 00:43:11 +1300

    done my first BDD

--- CucumberLearning/features/step_definitions/steps.rb
+++ CucumberLearning/features/step_definitions/steps.rb
@@ -1,11 +1,11 @@
 require 'caffeinate'
 
 
-
 Given("Maria orders ${int} of coffee from Li") do |int|
-  @maria = Cucumber.new
+  @maria = Customer.new
   @li = Associate.new
-  price = int
+  @price = int
+  @card_minimum = 2
   @maria.orders
 end
 
@@ -14,5 +14,10 @@ When("Maria pays with a credit card") do
 end
 
 Then("Li should process the payment") do
-  expect(@li.process_payment).to include(true)
+  expect(@li.process_payment(@price, @card_minimum)).to include(true)
+end
+
+Then("Li should not process the payment") do
+  expect(@li.process_payment(@price, @card_minimum)).to include(false)
+  expect(@li.process_payment(@price, @card_minimum)).to include('request_cash')
 end
